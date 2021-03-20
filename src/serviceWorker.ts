02c5f53e433a09c3612d5ab1845c2506d5841879
Alexandr Tretiakov <itretiy@gmail.com>
2021-03-20 13:39:22 +0200

    update react and fix serviceWorker.ts ts err

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -26,7 +26,8 @@ type Config = {
 };
 
 export function register(config?: Config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  // NOTE: @see {@link https://github.com/facebook/create-react-app/issues/8863}
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator && process.env.PUBLIC_URL) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
